dee21be5a2bf168e8163d4c13b5920b92a5b7fc8
Zenab Behlim <behlim.zenab@gmail.com>
2026-07-03 18:41:54 +0530

    Backend: add README and initial code

--- models/Task.js
+++ models/Task.js
@@ -2,6 +2,9 @@ import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  description: { type: String },   // optional
+  priority: { type: String },      // optional
+  dueDate: { type: Date },         // optional
   status: { type: String, default: 'Incomplete' }
 });
 
